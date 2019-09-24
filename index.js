e7dbb66d3858467b7c960e1a8a8599beb10146f2
Dimitrie Straoanu <dimitrie.straoanu@gmail.com>
2019-09-24 13:48:35 +0300

    scroll fixed

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+window.addEventListener('scroll', checkPosition);
+
 document.querySelector('.closeBtn').addEventListener('click', toggleMenu);
 
 document.querySelectorAll('.link').forEach(link => {
-    link.addEventListener('click', selectLink)
+    link.addEventListener('click', linkClicked)
 });
 
 function toggleMenu() {
@@ -9,8 +11,27 @@ function toggleMenu() {
     document.querySelectorAll('.closeBtn i').forEach(i => i.classList.toggle('hidden'));
 }
 
-function selectLink() {
-    document.querySelectorAll('.link').forEach(link => link.classList.remove('selected'));
-    this.classList.add('selected');
+function linkClicked() {    
     location.href = '#' + this.innerHTML.toLowerCase();
+}
+
+function checkPosition(){
+    const aboutHeight = Math.floor(document.querySelector('#about').getBoundingClientRect().height);
+    const workHeight = Math.floor(document.querySelector('#work').getBoundingClientRect().height);
+    const resumeHeight = Math.floor(document.querySelector('#resume').getBoundingClientRect().height);
+    const contactHeight = Math.floor(document.querySelector('#contact').getBoundingClientRect().height);
+    if(window.pageYOffset<aboutHeight) selectLink('About');
+    else if(window.pageYOffset>=aboutHeight && window.pageYOffset<aboutHeight+workHeight-1) selectLink('Work');
+    else if(window.pageYOffset>=aboutHeight+workHeight && window.pageYOffset<aboutHeight+workHeight+resumeHeight-1) selectLink('Resume');
+    else if(window.pageYOffset>=aboutHeight+workHeight+resumeHeight && window.pageYOffset<aboutHeight+workHeight+resumeHeight+contactHeight-1) selectLink('Contact');
+    console.log(window.pageYOffset);
+    console.log(aboutHeight);
+}
+
+function selectLink(linkName){
+    document.querySelectorAll('.link').forEach(link =>{
+       if(link.innerHTML === linkName) link.classList.add('selected');
+       else link.classList.remove('selected')
+    });
+        
 }
